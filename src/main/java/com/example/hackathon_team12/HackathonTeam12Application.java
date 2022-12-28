856aba8efec49e38679e91ccf29663199775da74
Jeongdonghun <arrrrr180@gmail.com>
2022-12-29 05:00:31 +0900

    Initial commit

---
+++ src/main/java/com/example/hackathon_team12/HackathonTeam12Application.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_team12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeam12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonTeam12Application.class, args);
+    }
+
+}
