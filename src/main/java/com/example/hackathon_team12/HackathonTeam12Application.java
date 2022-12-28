33d3c3333680af478939926688e24d16ed9282ce
Jeongdonghun <arrrrr180@gmail.com>
2022-12-29 05:32:49 +0900

    feat : database

--- src/main/java/com/example/hackathon_team12/HackathonTeam12Application.java
+++ src/main/java/com/example/hackathon_team12/HackathonTeam12Application.java
@@ -2,8 +2,10 @@ package com.example.hackathon_team12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HackathonTeam12Application {
 
     public static void main(String[] args) {
--- src/test/java/com/example/hackathon_team12/HackathonTeam12ApplicationTests.java
+++ src/test/java/com/example/hackathon_team12/HackathonTeam12ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.hackathon_team12;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,23 @@ class HackathonTeam12ApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void jasypt() {
+        String a ="AV";
+        String b="jdbc:mysql://springboot-database.c0tg3iklfpiz.ap-northeast-2.rds.amazonaws.com:3306/mydatabase";
+
+
+        System.out.println("안뇽");
+        System.out.println(jasyptEncoding(a));
+        System.out.println(jasyptEncoding(b));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "my_jasypt_key";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
 }
