b62da35ef9b8643bdb4f6713ed83a33e2768389c
Jeongdonghun <arrrrr180@gmail.com>
2022-12-29 20:47:32 +0900

    feat : 카카오 로그인

---
+++ src/main/java/com/example/hackathon_team12/controller/KakaoLoginController.java
@@ -0,0 +1,26 @@
+package com.example.hackathon_team12.controller;
+
+import com.example.hackathon_team12.dto.responseDto.GlobalResponseDto;
+import com.example.hackathon_team12.service.KakaoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class KakaoLoginController {
+
+    private final KakaoService kakaoService;
+
+    @GetMapping("member/signup/kakao")
+    public GlobalResponseDto<?> kakaoLogin(
+            @RequestParam(value = "code") String code, HttpServletResponse response) throws JsonProcessingException {
+        return kakaoService.kakaoLogin(code, response);
+    }
+}
---
+++ src/main/java/com/example/hackathon_team12/service/KakaoService.java
@@ -0,0 +1,192 @@
+package com.example.hackathon_team12.service;
+
+import com.example.hackathon_team12.domain.Member;
+import com.example.hackathon_team12.domain.RefreshToken;
+import com.example.hackathon_team12.domain.SocialAccessToken;
+import com.example.hackathon_team12.dto.SocialUserInfoDto;
+import com.example.hackathon_team12.dto.TokenDto;
+import com.example.hackathon_team12.dto.responseDto.GlobalResponseDto;
+import com.example.hackathon_team12.exception.CustomException;
+import com.example.hackathon_team12.exception.ErrorCode;
+import com.example.hackathon_team12.repository.MemberRepository;
+import com.example.hackathon_team12.repository.RefreshTokenRepository;
+import com.example.hackathon_team12.repository.SocialAccessTokenRepository;
+import com.example.hackathon_team12.security.jwt.JwtUtil;
+import com.example.hackathon_team12.security.jwt.UserDetailsImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class KakaoService {
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirect_uri;
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+    private String user_info_uri;
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String token_uri;
+
+    public final MemberRepository memberRepository;
+    public final RefreshTokenRepository refreshTokenRepository;
+    private final SocialAccessTokenRepository socialAccessTokenRepository;
+    public final JwtUtil jwtUtil;
+
+    public GlobalResponseDto<?> kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
+
+        //인가코드를 통해 access_token 발급받기
+        String accessToken = issuedAccessToken(code);
+
+        //access_token을 통해 사용자 정보가져오기
+        SocialUserInfoDto socialUserInfoDto = getKakaoUserInfo(accessToken);
+
+        //사용자정보를 토대로 가입진행하기(일단 DB에 저장이 되어있는지 확인후)
+        Member member = saveMember(socialUserInfoDto,accessToken);
+
+        //강제 로그인 처리
+        forceLoginUser(member);
+
+        //리프레쉬, 액세스 토큰 만들기
+        //토큰 발급후 response
+        createToken(member,response);
+
+        return GlobalResponseDto.success(socialUserInfoDto, "로그인이 완료되었습니다");
+    }
+
+    //인가코드를 통해 access_token 발급받기
+    public String issuedAccessToken(String code) throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP Body 생성
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", kakaoClientId);
+        body.add("redirect_uri", redirect_uri);
+        body.add("code", code);
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(body, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
+                token_uri,
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+        String accessToken = jsonNode.get("access_token").asText();
+
+        return accessToken;
+    }
+
+    //access_token을 통해 사용자 정보가져오기
+    private SocialUserInfoDto getKakaoUserInfo(String accessToken) throws JsonProcessingException {
+        // HTTP Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
+                user_info_uri,
+                HttpMethod.POST,
+                kakaoUserInfoRequest,
+                String.class
+        );
+
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        Long id = jsonNode.get("id").asLong();
+        String nickname = jsonNode.get("properties")
+                .get("nickname").asText();
+        String email = jsonNode.get("kakao_account")
+                .get("email").asText();
+
+        return new SocialUserInfoDto(id, nickname, email);
+    }
+
+    //사용자정보를 토대로 가입진행하기(일단 DB에 저장이 되어있는지 확인후)
+    public Member saveMember(SocialUserInfoDto socialUserInfoDto,String accessToken) {
+        Member kakaoMember = memberRepository.findByEmail("k_"+socialUserInfoDto.getEmail()).orElse(null);
+
+        //없다면 저장
+        if (kakaoMember == null) {
+
+            Member member = Member.builder().
+                    email("k_" + socialUserInfoDto.getEmail())
+                    .accountName(socialUserInfoDto.getNickname())
+                    .isAccepted(false)
+                    .isDeleted(false)
+                    .build();
+
+            memberRepository.save(member);
+            return member;
+        }
+
+        SocialAccessToken socialAccessToken = new SocialAccessToken(accessToken,"k_" + socialUserInfoDto.getEmail(),"kakao");
+        socialAccessTokenRepository.save(socialAccessToken);
+
+        //있다면 member 반환
+        return kakaoMember;
+    }
+
+    public void forceLoginUser(Member member) {
+        UserDetails userDetails = new UserDetailsImpl(member);
+        if (member.getIsDeleted().equals(true)) {
+            throw new CustomException(ErrorCode.DELETED_USER_EXCEPTION);
+        }
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    public void createToken(Member member,HttpServletResponse response){
+        TokenDto tokenDto = jwtUtil.createAllToken(member.getEmail());
+
+        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByAccountEmail(member.getEmail());
+
+        if (refreshToken.isPresent()) {
+            refreshTokenRepository.save(refreshToken.get().updateToken(tokenDto.getRefreshToken()));
+        } else {
+            RefreshToken newToken = new RefreshToken(tokenDto.getRefreshToken(), member.getEmail());
+            refreshTokenRepository.save(newToken);
+        }
+
+        setHeader(response, tokenDto);
+    }
+
+    public void setHeader(HttpServletResponse response, TokenDto tokenDto) {
+        response.addHeader(JwtUtil.ACCESS_TOKEN, tokenDto.getAccessToken());
+        response.addHeader(JwtUtil.REFRESH_TOKEN, tokenDto.getRefreshToken());
+    }
+}
